a7e164ea3e4c528e6b48c46f053c35dbd386b009
codemasterjoel <codemaster19@gmail.com>
2026-05-27 16:01:00 -0400

    actualizacion de constancia

--- resources/views/livewire/reportes/ficha.blade.php
+++ resources/views/livewire/reportes/ficha.blade.php
@@ -7,18 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    <table style="width: 100%; border: 0px;">
-		<tr class="mb-4"><td><img style="width:80%" src="img/alcaldia.png"></td><td style="text-align: right;"><img style="width:50%; display:block; margin-left:auto;" src="img/fecea.png"></td></tr>
-        <tr><td style="text-align: left; width: 33%"></td><td style="text-align: right; width: 64%">Nº de Registro: FECEA-{{$empresa->codigo}}/{{$empresa->created_at->format('Y')}}</td></tr>
+    <table style="width: 100%; border: 0px">
+		<tr style="margin-bottom: 10px;"><td><img style="width:80%" src="img/alcaldia.png"></td><td style="text-align: right;"><img style="width:50%; display:block; margin-left:auto;" src="img/fecea.png"></td></tr>
+        <tr style="margin-bottom: 10px;"><td style="text-align: left; width: 33%"></td><td style="text-align: right; width: 64%">Nº de Registro: FECEA-{{$empresa->codigo}}/{{$empresa->created_at->format('Y')}}</td></tr>
     </table>
-    <table style="width: 100%; border: 0px;" align="center">
+    <table style="width: 100%; border: 0px;" align="center" class="mb-20">
         <tr><td align="center"><h3>CONSTANCIA DE REGISTRO</h3></td></tr>
     </table>
-    <table style="text-align: center; width: 100%">
+    <table style="text-align: center; width: 100%" class="-mt-10" class="mb-20">
         <tbody>
 			{{-- <tr><td style="text-align:justify"><br><br></td></tr> --}}
 			<tr>
-				<td style="text-align:justify">
+				<td style="text-align:justify; line-height: 2;">
 					En atención a la receptoría <b>Nº R-{{$empresa->codigo}}</b> de fecha <b>{{$empresa->created_at->format('d/m/Y')}}</b>, mediante la cual solicita la inscripción en el <b>Registro Municipal Ambiental</b> de la denominación comercial 
 					<b style="text-transform: uppercase;">{{$empresa->nombre}}</b>, con Registro Fiscal <b>{{$empresa->tipoRIF}}-{{$empresa->rif}}</b>;
 					ubicada en <b style="text-transform: uppercase;">{{$empresa->direccion}}</b>; La Fundación para la Economía Circular y las Energías Alternativas; cumple en participarle que de acuerdo al informe de inspección realizado por personal técnico 
@@ -30,16 +30,20 @@
 					debiendo dar cumplimiento a las siguientes condiciones:
 				</td>
 			</tr>
+		</tbody>
+	</table>
+	<table style="text-align: center; width: 100%">
+		<tbody>
 			<tr>
 				<td style="text-align:justify">
 					<ul>
 						{{-- <ol>1.	El transporte de residuos y desechos sólidos especiales (reciclables), se realizará conforme al contenido de la “Hoja de seguimiento” y la respectiva autorización emitida por este despacho.</ol> --}}
-						<ol>1.	<b>ORIGEN DE LOS MATERIALES:</b>Se prohíbe estrictamente la comercialización, adquisición, almacenamiento o transporte de residuos y desechos sólidos especiales (ferrosos o no ferrosos) pertenecientes a organismos estratégicos del Estado (CANTV, CORPOELEC, HIDROCAPITAL, MOPVI, entre otros), salvo que la empresa presente la autorización expresa y certificada del ente titular de dichos bienes.</ol>
-						<ol>2.	<b>CUMPLIMIENTO NORMATIVO DE TRANSPORTE:</b>Las unidades de transporte empleadas para la actividad deben cumplir con la Ley de Transporte Terrestre, su Reglamento, y las Normas COVENIN vigentes. Queda prohibido el uso de vehículos de carga de gran envergadura (gandolas) que dificulten la movilidad urbana en las zonas residenciales autorizadas.</ol>
-						<ol>3.	El presente registro no excluye cualesquiera otras medidas legales tendiente a evitar los actos violatorios al cumplimiento de las normas y leyes que rigen la materia.</ol>
-						<ol><b>4.	REGISTRO VÁLIDO POR TRES (2) AÑOS A PARTIR DE LA FECHA DE SU EMISIÓN.<b></ol>
-						<ol><b>5.	El Personal que labora en la empresa arriba identificada, deberá formarse en la Escuela de Economía Circular y Energía Alternativa en un plazo de 30 días, sin prorroga.</b></ol>
-						<ol>6.	El incumplimiento de las condiciones aquí establecidas dará lugar a la anulación o suspensión del presente registro, y a la apertura del correspondiente procedimiento administrativo.</ol>
+						<ol style="line-height: 2;">1.	<b>ORIGEN DE LOS MATERIALES:</b>Se prohíbe estrictamente la comercialización, adquisición, almacenamiento o transporte de residuos y desechos sólidos especiales (ferrosos o no ferrosos) pertenecientes a organismos estratégicos del Estado (CANTV, CORPOELEC, HIDROCAPITAL, MOPVI, entre otros), salvo que la empresa presente la autorización expresa y certificada del ente titular de dichos bienes.</ol>
+						<ol style="line-height: 2;">2.	<b>CUMPLIMIENTO NORMATIVO DE TRANSPORTE:</b>Las unidades de transporte empleadas para la actividad deben cumplir con la Ley de Transporte Terrestre, su Reglamento, y las Normas COVENIN vigentes. Queda prohibido el uso de vehículos de carga de gran envergadura (gandolas) que dificulten la movilidad urbana en las zonas residenciales autorizadas.</ol>
+						<ol style="line-height: 2;">3.	El presente registro no excluye cualesquiera otras medidas legales tendiente a evitar los actos violatorios al cumplimiento de las normas y leyes que rigen la materia.</ol>
+						<ol style="line-height: 2;"><b>4.	REGISTRO VÁLIDO POR TRES (2) AÑOS A PARTIR DE LA FECHA DE SU EMISIÓN.<b></ol>
+						<ol style="line-height: 2;"><b>5.	El Personal que labora en la empresa arriba identificada, deberá formarse en la Escuela de Economía Circular y Energía Alternativa en un plazo de 30 días, sin prorroga.</b></ol>
+						<ol style="line-height: 2;">6.	El incumplimiento de las condiciones aquí establecidas dará lugar a la anulación o suspensión del presente registro, y a la apertura del correspondiente procedimiento administrativo.</ol>
 					</ul>
 				</td>
 			</tr>
